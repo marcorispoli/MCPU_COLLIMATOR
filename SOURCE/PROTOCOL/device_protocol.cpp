cca61e6ff11c0057d429e19c080aca66992f0aa3
marcorispoli <marco.rispoli@gmail.com>
2023-06-15 14:32:00 +0200

    Avanzamenti

--- SOURCE/PROTOCOL/device_protocol.cpp
+++ SOURCE/PROTOCOL/device_protocol.cpp
@@ -2,7 +2,7 @@
 #include "can_device_protocol.h"
 
 
-deviceProtocol::deviceProtocol(void):canDeviceProtocol(Application::DEVICE_ID, SYSCONFIG->getParam<QString>(SYS_CAN_PROCESS_PARAM, SYS_PROCESS_IP) ,  SYSCONFIG->getParam<uint>(SYS_CAN_PROCESS_PARAM, SYS_PROCESS_PORT))
+deviceProtocol::deviceProtocol(void):canDeviceProtocol(Application::DEVICE_ID , SYSCONFIG->getParam<QString>(SYS_CAN_PROCESS_PARAM, SYS_PROCESS_IP) ,  SYSCONFIG->getParam<uint>(SYS_CAN_PROCESS_PARAM, SYS_PROCESS_PORT))
 {
 
     canDeviceProtocolFrame::CAN_REGISTER_t reg0;
